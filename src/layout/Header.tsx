0fb77fe529f096e2e8d5f05e65a07353ca48c592
nasrullohfajar <nasrullohfajar10@gmail.com>
2026-02-18 20:12:15 +0700

    style: adjust header padding

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -2,7 +2,7 @@ import { FaHouse } from "react-icons/fa6";
 
 const Header = () => {
   return (
-    <div className="flex items-center px-3 xl:px-4 py-4 xl:py-6 bg-white rounded-sm shadow-xs">
+    <div className="flex items-center px-3 xl:px-6 py-4 lg:py-6 bg-white rounded-sm shadow-xs">
       <span>
         <FaHouse size={16} className="mb-0.5" />
       </span>
