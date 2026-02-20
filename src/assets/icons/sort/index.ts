bb5573b616d88f71f0b47162ca356a2c3ed85278
nasrullohfajar <nasrullohfajar10@gmail.com>
2026-02-20 14:23:14 +0700

    chore: add custom sort icon

---
+++ src/assets/icons/sort/index.ts
@@ -0,0 +1,6 @@
+// src/assets/icons/sort/index.ts
+import SortIcon from "./sort-icon.svg?react";
+import SortUpIcon from "./sort-icon-up.svg?react";
+import SortDownIcon from "./sort-icon-down.svg?react";
+
+export { SortIcon, SortUpIcon, SortDownIcon };
