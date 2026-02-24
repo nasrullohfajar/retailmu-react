7b2cffda011524bfe2f4bb62e81622f3a670a758
nasrullohfajar <nasrullohfajar10@gmail.com>
2026-02-24 15:52:50 +0700

    fix: remove typo at axios instance

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   headers: {
-    "Content-Type": "applicateion/json",
+    "Content-Type": "application/json",
   },
   withCredentials: true,
 });
